116dda2e78646179b0078655c544d00113b4bba0
David Ma <maxxx818@umn.edu>
2021-02-17 03:05:21 -0600

    started testing files

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
 CC = g++
 CFLAGS = -lrt -g
 DEPS = TCB.h uthread.h
-OBJ = TCB.o uthread.o main.o
+OBJ = TCB.o uthread.o test.o
 
 %.o: %.cpp $(DEPS)
-	$(CC) -c -o $@ $< $(CFLAGS) 
+	$(CC) -c -o $@ $< $(CFLAGS)
 
-uthread-demo: $(OBJ)
+test: $(OBJ)
 	$(CC) -o $@ $^ $(CFLAGS)
 
 .PHONY: clean
--- test.cpp
+++ test.cpp
@@ -1,8 +1,38 @@
 #include "uthread.h"
+#include <unistd.h>
 #include <iostream>
 
 using namespace std;
 
+void *worker(void *arg) {
+    for (int i=0; i<4; i++) {
+      sleep(1000);
+      cout << i;
+    }
+    cout << endl;
+    return nullptr;
+}
+
 int main(int argc, char *argv[]) {
+  int quantum_usecs = 1000;
+  int thread_count = 5;
+  int thread_args = 5;
+  int *threads = new int[thread_count];
+
+  int ret = uthread_init(quantum_usecs);
+  if (ret != 0) {
+      cerr << "uthread_init FAIL!\n" << endl;
+      exit(1);
+  }
+
+  for (int i = 0; i < thread_count; i++) {
+      int tid = uthread_create(worker, &thread_args);
+      threads[i] = tid;
+  }
+
+  
 
 }
+
+// Test init, yield, Create
+// interrupts
